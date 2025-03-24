0372a5f10c63e60d7c35f7e12840d176aa79945c
abhikunagu <abhikunagu27@gmail.com>
2025-03-24 06:18:36 +0000

    Added Terraform configuration for Azure Backup

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-terraform {
+terraform { 
   required_version = ">= 1.3.0"
 
   required_providers {
@@ -8,17 +8,17 @@ terraform {
     }
   }
 
- backend "azurerm" {
-    resource_group_name   = "backupcode"
-    storage_account_name  = "backupdatastorage1"
-    container_name        = "backup"
-    key                   = "infra/terraform.tfstate"
-    subscription_id       = "fb656642-6401-44d5-9de1-14bda10d53e5"  # Add this explicitly
+  backend "azurerm" {
+    resource_group_name  = "backupcode"
+    storage_account_name = "backupdatastorage1"
+    container_name       = "backup"
+    key                  = "infra/terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "fb656642-6401-44d5-9de1-14bda10d53e5"
 }
 
 # Create Resource Group
@@ -27,9 +27,15 @@ resource "azurerm_resource_group" "backupcode" {
   location = "East US"
 }
 
-# Create Storage Account for Backup
+# Create Storage Account with a unique name
+resource "random_string" "storage_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "azurerm_storage_account" "backup_storage" {
-  name                     = "backupdatastorage1"
+  name                     = "backup${random_string.storage_suffix.result}"
   resource_group_name      = azurerm_resource_group.backupcode.name
   location                 = azurerm_resource_group.backupcode.location
   account_tier             = "Standard"
@@ -68,19 +74,10 @@ resource "azurerm_backup_policy_vm" "vm_backup_policy" {
   }
 }
 
-# Enable Backup for an Azure VM
+# Enable Backup for an Azure VM (Dynamic VM ID)
+variable "vm_id" {
+  description = "The ID of the Azure Virtual Machine to back up"
+  type        = string
+}
+
 resource "azurerm_backup_protected_vm" "vm_backup" {
-  resource_group_name = azurerm_resource_group.backupcode.name
-  recovery_vault_name = azurerm_recovery_services_vault.backup_vault.name
-  source_vm_id        = "/subscriptions/fb656642-6401-44d5-9de1-14bda10d53e5/resourceGroups/backupcode/providers/Microsoft.Compute/virtualMachines/backupvm"
-  backup_policy_id    = azurerm_backup_policy_vm.vm_backup_policy.id
-}
-
-# Outputs
-output "storage_account_name" {
-  value = azurerm_storage_account.backup_storage.name
-}
-
-output "backup_vault_name" {
-  value = azurerm_recovery_services_vault.backup_vault.name
-}
